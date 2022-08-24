2014820d437b9f9e633e0418bb62da572f268dbb
Bagheerathan <70306720+Bagheerathan@users.noreply.github.com>
2022-08-24 14:47:07 +0530

    Local community
    local community page copy.  add new card design

---
+++ assets/js/Community.js
@@ -0,0 +1,41 @@
+VanillaTilt.init(document.querySelector(".glassmorphic-card1"), {
+    max: 15,
+    speed: 200,
+    glare: true,
+    "max-glare": 1,
+  });
+  
+  VanillaTilt.init(document.querySelector(".card2"), {
+    max: 15,
+    speed: 200,
+    glare: true,
+    "max-glare": 1,
+  });
+
+  VanillaTilt.init(document.querySelector(".card3"), {
+    max: 15,
+    speed: 200,
+    glare: true,
+    "max-glare": 1,
+  });
+  
+  VanillaTilt.init(document.querySelector(".card4"), {
+    max: 15,
+    speed: 200,
+    glare: true,
+    "max-glare": 1,
+  });
+  
+  VanillaTilt.init(document.querySelector(".card5"), {
+    max: 15,
+    speed: 200,
+    glare: true,
+    "max-glare": 1,
+  });
+
+  VanillaTilt.init(document.querySelector(".card6"), {
+    max: 15,
+    speed: 200,
+    glare: true,
+    "max-glare": 1,
+  });
