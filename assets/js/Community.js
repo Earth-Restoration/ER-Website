1adb8c4a2cac16244290fff795006a29837385fd
Bagheerathan <70306720+Bagheerathan@users.noreply.github.com>
2022-08-24 21:30:15 +0530

    global community

--- assets/js/Community.js
+++ assets/js/Community.js
@@ -1,41 +1,29 @@
 VanillaTilt.init(document.querySelector(".glassmorphic-card1"), {
     max: 15,
-    speed: 200,
-    glare: true,
-    "max-glare": 1,
+    speed: 200,  
   });
   
   VanillaTilt.init(document.querySelector(".card2"), {
     max: 15,
     speed: 200,
-    glare: true,
-    "max-glare": 1,
   });
 
   VanillaTilt.init(document.querySelector(".card3"), {
     max: 15,
     speed: 200,
-    glare: true,
-    "max-glare": 1,
   });
   
   VanillaTilt.init(document.querySelector(".card4"), {
     max: 15,
     speed: 200,
-    glare: true,
-    "max-glare": 1,
   });
   
   VanillaTilt.init(document.querySelector(".card5"), {
     max: 15,
     speed: 200,
-    glare: true,
-    "max-glare": 1,
   });
 
   VanillaTilt.init(document.querySelector(".card6"), {
     max: 15,
     speed: 200,
-    glare: true,
-    "max-glare": 1,
   });
