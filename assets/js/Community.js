6579fa40d62560c1057be0cfc0ab2aca906f1763
Bagheerathan <70306720+Bagheerathan@users.noreply.github.com>
2022-08-25 17:26:17 +0530

    background image and some changes

--- assets/js/Community.js
+++ assets/js/Community.js
@@ -27,3 +27,33 @@ VanillaTilt.init(document.querySelector(".glassmorphic-card1"), {
     max: 15,
     speed: 200,
   });
+
+  VanillaTilt.init(document.querySelector(".card7"), {
+    max: 15,
+    speed: 200,
+  });
+
+  VanillaTilt.init(document.querySelector(".card8"), {
+    max: 15,
+    speed: 200,
+  });
+
+  VanillaTilt.init(document.querySelector(".card9"), {
+    max: 15,
+    speed: 200,
+  });
+
+  VanillaTilt.init(document.querySelector(".card10"), {
+    max: 15,
+    speed: 200,
+  });
+
+  VanillaTilt.init(document.querySelector(".card11"), {
+    max: 15,
+    speed: 200,
+  });
+
+  VanillaTilt.init(document.querySelector(".card12"), {
+    max: 15,
+    speed: 200,
+  });
